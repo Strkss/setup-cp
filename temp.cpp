088ee8414870beab5b68045914c5d0e666107e93
Strkss <88977475+Strkss@users.noreply.github.com>
2024-09-24 17:54:11 +0700

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,79 @@
+/*
+Tips:
+    1.int? long long?
+    2.don't submit wrong answer
+    3.figure out logic first, then start writing please
+    4.know about the range
+    5.check if you have to input t or not
+    6.modulo of negative numbers is not a%b, it is a%b + abs(b)
+*/
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <locale>
+#include <map>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <cstring>
+#include <unordered_map>
+#include <cmath>
+#include <array>
+#include <cassert>
+#include <random>
+#include <chrono>
+using namespace std;
+
+#define LIFESUCKS cin.tie(0)->sync_with_stdio(0);cin.exceptions(cin.failbit);
+#define fi first
+#define se second
+#define bit(n,i) ((n>>i)&1)
+#define valid(i,n) (i>=1&&i<=n)
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<int> vi;
+
+const ll MOD = 1e9+7;
+
+int main(){
+    LIFESUCKS;
+
+
+
+    return 0;
+}
+
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?)
+    * do smth instead of nothing and stay organised
+    * WRITE STUFF DOWN
+    * DON'T GET STUCK ON ONE APPROACH
+*/
